11f9c2a583284c7479f3af23e386669d68aee233
Snigdha Regmi <sregmi@NORD.LOCAL>
2019-02-27 20:15:46 -0700

    social card

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import './footer.css';
+
+function Footer() {
+    return (
+        <div className="footer-class">
+        
+        </div>
+    );
+};
+export default Footer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+
+function HeaderTitle() {
+    return (
+        <div className = "header-title">
+            <div className = "lambda-school">Lambda School</div>
+            <div className ="symbol">@LambdaSchool</div>
+            <div className = "date">.26 jan</div>
+        </div>
+    );
+};
+
+export default HeaderTitle;
+
+
